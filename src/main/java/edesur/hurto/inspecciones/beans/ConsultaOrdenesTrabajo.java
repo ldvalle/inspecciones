0cf0979640cf0d03adc4118310122541c1f0bf08
ldvalle <lukrecio01@hotmail.com>
2022-04-07 10:12:25 -0300

    Update ConsultaOrdenesTrabajo.java
    Se agregó los cortes y reposiciones

--- src/main/java/edesur/hurto/inspecciones/beans/ConsultaOrdenesTrabajo.java
+++ src/main/java/edesur/hurto/inspecciones/beans/ConsultaOrdenesTrabajo.java
@@ -61,6 +61,12 @@ public class ConsultaOrdenesTrabajo {
                 //stmt.setLong(1, nroCliente);
                 stmt.setLong(1, auxNroCliente);
                 stmt.executeUpdate();
+
+                stmt=null;
+                stmt = conn.prepareStatement(INS_CORREP_EORDER);
+                stmt.setLong(1, nroCliente);
+                stmt.executeUpdate();
+
             }else{
                 stmt=null;
                 stmt = conn.prepareStatement(INS_INSPE_T1);
@@ -77,6 +83,20 @@ public class ConsultaOrdenesTrabajo {
                 stmt.setLong(1, nroCliente);
                 stmt.executeUpdate();
 
+                stmt=null;
+                stmt = conn.prepareStatement(INS_CORTE_MAC);
+                stmt.setLong(1, nroCliente);
+                stmt.executeUpdate();
+
+                stmt=null;
+                stmt = conn.prepareStatement(INS_REPO_MAC);
+                stmt.setLong(1, nroCliente);
+                stmt.executeUpdate();
+
+                stmt=null;
+                stmt = conn.prepareStatement(INS_CORREP_EORDER);
+                stmt.setLong(1, nroCliente);
+                stmt.executeUpdate();
             }
 
             try(PreparedStatement stmt2 = conn.prepareStatement(SEL_CONSULTA)) {
@@ -145,6 +165,33 @@ public class ConsultaOrdenesTrabajo {
             "AND e.nro_solicitud = s.nro_solicitud " +
             "AND e.fecha_finalizacion IS NOT NULL ";
 
+    private static final String INS_CORTE_MAC = "INSERT INTO tempo1 (numero_cliente, fecha_evento, tipo_evento, valor_evento) " +
+            "SELECT numero_cliente, DATE(fecha_corte), 'Susp',1 " +
+            "FROM correp " +
+            "WHERE numero_cliente = ? " +
+            "AND fecha_corte IS NOT NULL " +
+            "AND accion_corte IN ('00', '14', '15', '18', '20',  '21','22','23', " +
+            "'24','25','26','27','31','32','33','36','37') ";
+
+    private static final String INS_REPO_MAC = "INSERT INTO tempo1 (numero_cliente, fecha_evento, tipo_evento, valor_evento) " +
+            "SELECT numero_cliente, DATE(fecha_reposicion), 'Rehab',1 " +
+            "FROM correp " +
+            "WHERE numero_cliente = ? " +
+            "AND fecha_reposicion IS NOT NULL " +
+            "AND accion_rehab IN ('00','15','20','17','18','20','21','22','23', " +
+            "'24','31','32','36','37') ";
+
+    private static final String INS_CORREP_EORDER = "INSERT INTO tempo1 (numero_cliente, fecha_evento, tipo_evento, valor_evento) " +
+            "SELECT numero_cliente, DATE(fecha_fin_tarea), " +
+            "CASE " +
+            "   WHEN TRIM(tipo_tdc) = 'CORTE' THEN 'Susp' " +
+            "   ELSE 'Rehab' " +
+            "END, 1 " +
+            "FROM eo_corte_repo " +
+            "WHERE numero_cliente = ? " +
+            "AND ((TRIM(tipo_tdc) = 'CORTE' AND codigo_accion IN ('00', '14', '15')) " +
+            "    OR " +
+            "    (TRIM(tipo_tdc) = 'REPO' AND codigo_accion IN ('00', '13', '15', '20', '21', '31'))) ";
 
     private static final String SEL_CONSULTA = "SELECT numero_cliente, fecha_evento, trim(tipo_evento), valor_evento " +
             "FROM tempo1 " +
